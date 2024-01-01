01cac250ec1fc4beb4b9019e6d602971bda70eaf
omidtavakoli901 <omid37268082@gamil.com>
2024-01-01 21:24:42 +0330

    change style faq

--- public/script/app.js
+++ public/script/app.js
@@ -1,14 +1,28 @@
 const menu = document.querySelector(".menu")
 const hidemenu = document.querySelector(".hidemenu")
 const showmenu = document.querySelector(".showmenu")
+const question = document.querySelector(".text-question")
+const boxQuestion = document.querySelector(".question")
+
+
 
 function toggleAnswer(id) {
   const answer = document.getElementById("answer" + id);
   console.log(answer)
   if (answer.classList == "hidden" ) {
     answer.classList.remove("hidden");
+    question.classList.add("text-pale-blue");
+    question.classList.remove("text-quiz-faq");
+    boxQuestion.classList.add("rounded-t-md");
+    boxQuestion.classList.remove("rounded-md");
+    
+
   } else {
     answer.classList.add("hidden");
+    question.classList.remove("text-pale-blue");
+    question.classList.add("text-quiz-faq");
+    boxQuestion.classList.remove("rounded-t-md");
+    boxQuestion.classList.add("rounded-md");
   }
 }
 showmenu.addEventListener("click" , () => {
